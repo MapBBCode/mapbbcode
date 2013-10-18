cd089c7051083c73022773d929438dd403f9a4a7
Ilya Zverev <zverik@textual.ru>
2013-10-18 13:40:42 +0400

    update deps

--- build/deps.js
+++ build/deps.js
@@ -10,89 +10,95 @@ var deps = {
 		      'EditorSprites.js',
 		      'param/Param.Text.js'],
 		desc: 'User Interface: displaying and editing map bbcode',
-                deps: ['Core']
+		deps: ['Core']
 	},
 
-        Editor: {
-                src: ['MapBBCodeUI.Editor.js'],
-                desc: 'BBCode Editor',
-                deps: ['UI']
-        },
+	Editor: {
+		src: ['MapBBCodeUI.Editor.js'],
+		desc: 'BBCode Editor',
+		deps: ['UI']
+	},
 
-        ParamColor: {
-                src: ['param/Param.Color.js'],
-                desc: 'Color',
-                heading: 'Object Parameter Handlers'
-        },
+	ParamColor: {
+		src: ['param/Param.Color.js'],
+		desc: 'Color',
+		heading: 'Object Parameter Handlers'
+	},
 
-        ParamWidth: {
-                src: ['param/Param.Width.js'],
-                desc: 'Width',
-                noInclude: true
-        },
+	ParamWidth: {
+		src: ['param/Param.Width.js'],
+		desc: 'Width',
+		noInclude: true
+	},
 
-        LetterIcon: {
+	ParamMeasure: {
+		src: ['param/Param.Measure.js'],
+		desc: 'Length and Area (not a parameter)',
+		noInclude: true
+	},
+
+	LetterIcon: {
 		src: ['LetterIcon.js'],
 		desc: 'Optional LetterIcon',
-                heading: 'Plugins'
-        },
+		heading: 'Plugins'
+	},
 
-        Search: {
+	Search: {
 		src: ['Leaflet.Search.js'],
 		desc: 'Nominatim search control'
-        },
+	},
 
-        LayerSwitcher: {
-                src: ['config/StaticLayerSwitcher.js'],
-                desc: 'Layer switcher that is not a single button',
-                config: true
-        },
+	LayerSwitcher: {
+		src: ['config/StaticLayerSwitcher.js'],
+		desc: 'Layer switcher that is not a single button',
+		config: true
+	},
 
-        LayerList: {
-                src: ['config/LayerList.js'],
-                desc: 'List of layers to simplify configuration',
-                config: true
-        },
+	LayerList: {
+		src: ['config/LayerList.js'],
+		desc: 'List of layers to simplify configuration',
+		config: true
+	},
 
-        Configuration: {
-                src: ['config/MapBBCodeUI.Config.js',
-                      'FunctionButton.js'],
-                desc: 'MapBBCode UI configuration module',
-                heading: 'Configuration',
-                config: true,
-                deps: ['LayerSwitcher', 'LayerList']
-        },
+	Configuration: {
+		src: ['config/MapBBCodeUI.Config.js',
+		      'FunctionButton.js'],
+		desc: 'MapBBCode UI configuration module',
+		heading: 'Configuration',
+		config: true,
+		deps: ['LayerSwitcher', 'LayerList']
+	},
 
-        StringsEnglish: {
-                src: ['strings/English.js'],
-                desc: 'English',
-                heading: 'Translations',
-                deps: ['UI']
-        },
+	StringsEnglish: {
+		src: ['strings/English.js'],
+		desc: 'English',
+		heading: 'Translations',
+		deps: ['UI']
+	},
 
-        StringsRussian: {
-                src: ['strings/Russian.js'],
-                desc: 'Russian',
-                deps: ['UI'],
-                noInclude: true,
-                onlyIn: ['russian']
-        },
+	StringsRussian: {
+		src: ['strings/Russian.js'],
+		desc: 'Russian',
+		deps: ['UI'],
+		noInclude: true,
+		onlyIn: ['russian']
+	},
 
-        StringsEnglishC: {
-                src: ['strings/English.Config.js'],
-                desc: 'English for Configuration',
-                deps: ['Configuration'],
-                config: true
-        },
+	StringsEnglishC: {
+		src: ['strings/English.Config.js'],
+		desc: 'English for Configuration',
+		deps: ['Configuration'],
+		config: true
+	},
 
-        StringsRussianC: {
-                src: ['strings/Russian.Config.js'],
-                desc: 'Russian for Configuration',
-                deps: ['Configuration'],
-                noInclude: true,
-                config: true,
-                onlyIn: ['russian']
-        }
+	StringsRussianC: {
+		src: ['strings/Russian.Config.js'],
+		desc: 'Russian for Configuration',
+		deps: ['Configuration'],
+		noInclude: true,
+		config: true,
+		onlyIn: ['russian']
+	}
 };
 
 if (typeof exports !== 'undefined') {
