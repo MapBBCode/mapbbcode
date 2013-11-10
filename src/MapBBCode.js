bd36ebf2a58196d65667fefe2c7e0e32feeeb979
Ilya Zverev <zverik@textual.ru>
2013-11-11 00:22:47 +0400

    move MapBBCodeProcessor options to options object

--- src/MapBBCode.js
+++ src/MapBBCode.js
@@ -2,37 +2,38 @@
  * Map BBCode parser and producer. See BBCODE.md for description.
  */
 window.MapBBCodeProcessor = {
-    decimalDigits: 5,
-    brackets: '[]',
-	tagParams: false,
+    options: {
+        decimalDigits: 5,
+        brackets: '[]',
+        tagParams: false,
+    },
 
     _getRegExp: function() {
-        var openBr = this.brackets.substring(0, 1).replace(/([\[\({])/, '\\$1'),
-            closBr = this.brackets.substring(1, 2).replace(/([\]\)}])/, '\\$1');
+        var openBr = this.options.brackets.substring(0, 1).replace(/([\[\({])/, '\\$1'),
+            closBr = this.options.brackets.substring(1, 2).replace(/([\]\)}])/, '\\$1');
         var reCoord = '\\s*(-?\\d+(?:\\.\\d+)?)\\s*,\\s*(-?\\d+(?:\\.\\d+)?)',
             reParams = '\\((?:([a-zA-Z0-9,]*)\\|)?(|[\\s\\S]*?[^\\\\])\\)',
             reMapElement = reCoord + '(?:' + reCoord + ')*(?:\\s*' + reParams + ')?',
-            reMapOpeningTag = openBr + 'map(?:' + (this.tagParams ? '\\s+z=[\'"]([12]?\\d)[\'"](?:\\s+ll=[\'"]' + reCoord + '[\'"])?' : '=([12]?\\d)(?:,' + reCoord + ')?') + ')?' + closBr,
-            reMap = reMapOpeningTag + '(' + reMapElement + '(?:\\s*;' + reMapElement + ')*)?\\s*' + openBr + '/map' + closBr,
-            reMapC = new RegExp(reMap, 'i');
+            reMapOpeningTag = openBr + 'map(?:' + (this.options.tagParams ? '\\s+z=[\'"]([12]?\\d)[\'"](?:\\s+ll=[\'"]' + reCoord + '[\'"])?' : '=([12]?\\d)(?:,' + reCoord + ')?') + ')?' + closBr,
+            reMap = reMapOpeningTag + '(' + reMapElement + '(?:\\s*;' + reMapElement + ')*)?\\s*' + openBr + '/map' + closBr;
         return {
             coord: reCoord,
             params: reParams,
-			mapElement: reMapElement,
+            mapElement: reMapElement,
             map: reMap,
             mapCompiled: new RegExp(reMap, 'i')
         };
     },
 
-	// returns longest substring for determining a start of map bbcode, "[map" by default
-	getOpenTagSubstring: function() {
-		return this.brackets.substring(0, 1) + 'map';
-	},
+    // returns longest substring for determining a start of map bbcode, "[map" by default
+    getOpenTagSubstring: function() {
+        return this.options.brackets.substring(0, 1) + 'map';
+    },
 
-	// returns longest substring for determining an end of map bbcode, "[/map]" by default
-	getCloseTagSubstring: function() {
-		return this.brackets.substring(0, 1) + '/map' + this.brackets.substring(1, 2);
-	},
+    // returns longest substring for determining an end of map bbcode, "[/map]" by default
+    getCloseTagSubstring: function() {
+        return this.options.brackets.substring(0, 1) + '/map' + this.options.brackets.substring(1, 2);
+    },
 
     // Checks that bbcode string is a valid map bbcode
     isValid: function( bbcode ) {
@@ -56,11 +57,11 @@ window.MapBBCodeProcessor = {
 
         if( matches && matches[4] ) {
             var items = matches[4], itm,
-				reElementC = new RegExp('^\\s*(?:;\\s*)?(' + regExp.mapElement + ')');
+                reElementC = new RegExp('^\\s*(?:;\\s*)?(' + regExp.mapElement + ')'),
                 reCoordC = new RegExp('^' + regExp.coord);
 
-			itm = items.match(reElementC);
-			while( itm ) {
+            itm = items.match(reElementC);
+            while( itm ) {
                 var s = itm[1],
                     coords = [], m, text = '', params = [];
                 m = s.match(reCoordC);
@@ -69,14 +70,14 @@ window.MapBBCodeProcessor = {
                     s = s.substring(m[0].length);
                     m = s.match(reCoordC);
                 }
-				if( itm[6] )
-					params = itm[6].split(',');
-				if( typeof itm[7] === 'string' && itm[7].length > 0 )
-					text = itm[7].replace(/\\\)/g, ')').replace(/^\s+|\s+$/g, '');
+                if( itm[6] )
+                    params = itm[6].split(',');
+                if( typeof itm[7] === 'string' && itm[7].length > 0 )
+                    text = itm[7].replace(/\\\)/g, ')').replace(/^\s+|\s+$/g, '');
                 result.objs.push({ coords: coords, text: text, params: params });
 
-				items = items.substring(itm[0].length);
-				itm = items.match(reElementC);
+                items = items.substring(itm[0].length);
+                itm = items.match(reElementC);
             }
         }
 
@@ -87,9 +88,9 @@ window.MapBBCodeProcessor = {
     objectsToString: function( data ) {
         var mapData = '';
         if( data.zoom > 0 ) {
-            mapData = this.tagParams ? ' z="' + data.zoom + '"' : '=' + data.zoom;
+            mapData = this.options.tagParams ? ' z="' + data.zoom + '"' : '=' + data.zoom;
             if( data.pos )
-                mapData += this.tagParams ? ' ll="' + this._latLngToString(data.pos) + '"' : ',' + this._latLngToString(data.pos);
+                mapData += this.options.tagParams ? ' ll="' + this._latLngToString(data.pos) + '"' : ',' + this._latLngToString(data.pos);
         }
 
         var markers = [], paths = [], objs = data.objs || [];
@@ -113,13 +114,13 @@ window.MapBBCodeProcessor = {
             }
         }
 
-        var openBr = this.brackets.substring(0, 1),
-            closBr = this.brackets.substring(1, 2);
+        var openBr = this.options.brackets.substring(0, 1),
+            closBr = this.options.brackets.substring(1, 2);
         return markers.length || paths.length || mapData.length ? openBr + 'map' + mapData + closBr + markers.concat(paths).join('; ') + openBr + '/map' + closBr : '';
     },
 
     _latLngToString: function( latlng ) {
-        var mult = Math.pow(10, this.decimalDigits);
+        var mult = Math.pow(10, this.options.decimalDigits);
         return '' + (Math.round((latlng.lat || latlng[0]) * mult) / mult) + ',' + (Math.round((latlng.lng || latlng[1]) * mult) / mult);
     }
 };
